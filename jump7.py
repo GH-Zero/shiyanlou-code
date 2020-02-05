219823499a8cff601d255421c7647bdc74a876c3
zero <2269205119@qq.com>
2020-02-05 14:17:01 +0800

    first commit

---
+++ jump7.py
@@ -0,0 +1,7 @@
+q=0
+while q<100:
+        q+=1
+            if q%7==0 or q%10==7 or q//10==7:
+                        continue
+                        else:
+                                    print(q)
